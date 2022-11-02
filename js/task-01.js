0d0c52ca52d036fa04cf49522562010a0e234b57
Roman Gostylo <ronnik81@gmail.com>
2022-11-02 10:44:49 +0200

    Go!

--- js/task-01.js
+++
@@ -1,7 +0,0 @@
-const categories = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categories.length}`);
-
-categories.forEach(element => {
-    console.log(`Category: ${element.querySelector('h2').textContent}`);
-    console.log(`Elements: ${element.querySelectorAll('li').length}`);
-});
--- js/task-02.js
+++ js/task-02.js
@@ -6,13 +6,3 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
-const elements = ingredients.map(option => {
-  const element = document.createElement('li');
-  element.textContent = option;
-  element.classList.add('item');
-  return element;
-});
-
-const list = document.querySelector('#ingredients');
-list.append(...elements);
