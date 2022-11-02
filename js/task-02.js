9f3a849689292461a7bf37fab0005b7ff168e98b
Roman Gostylo <ronnik81@gmail.com>
2022-11-02 12:38:05 +0200

    Task 2 Done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createItems = [];
+
+ingredients.forEach(element => {
+  const createItem = document.createElement('li');
+  createItem.classList.add('item');
+  createItem.textContent = element;
+  createItems.push(createItem);
+})
+
+const listEl = document.querySelector('#ingredients');
+listEl.append(...createItems);
